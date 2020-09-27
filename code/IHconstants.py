25321d525aabd2ae82017aedca31f5b685a4a8ec
Michiel Tiller <michiel@tiller.nl>
2020-09-27 20:40:33 +0200

    gpl

--- code/IHconstants.py
+++ code/IHconstants.py
@@ -1,6 +1,20 @@
 # IntesisHome (AirconWithMe) interface strings
-# Written by Michiel Tiller (https://github.com/mchlt)
-# 
+#
+#
+#   Copyright (C) 2020 Michiel Tiller
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 UID_ON_OFF = 1
 UID_USER_MODE = 2
